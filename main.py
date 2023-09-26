fba488ed2afa8711d90e0484f7108075c750d23f
“javascriptseo” <seofreelance2021@outlook.com>
2023-09-25 22:59:32 -0700

    solved

--- main.py
+++ main.py
@@ -7,19 +7,17 @@ Output: Prints the number of left-handed students and the number of right-handed
 
 rightTotal = 0  # Number of right-handed students.
 leftTotal = 0  # Number of left-handed students.
-leftOrRight = ""
+leftOrRight = "R"
+leftOrRight = "R"
 
-leftOrRight = input("Enter an L if you are left-handed,a R if you are right-handed or X to quit.")
 
-# Write your loop here.
-while (leftOrRight == "L" or leftOrRight == "R"):
-    leftTotal = input("Enter an L if you are left handed, R if you are right ")
+while (leftOrRight== "L" or leftOrRight == "R"):
+    leftOrRight = input("Enter an L if you are left-handed,a R if you are right-handed or X to quit.")
     if leftOrRight == "L":
-        leftTotal = leftTotal + 1:
-     elif leftOrRight == "R":
-            rightTotal = rightTotal + 1
-            else:
-                break
+        leftTotal = leftTotal + 1
+    elif leftOrRight == "R":
+        rightTotal = rightTotal + 1
+
 
 # Output number of left or right-handed students.
 print("Number of left-handed students: " + str(leftTotal))
